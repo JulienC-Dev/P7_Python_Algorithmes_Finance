1c76d111f6bd3e66e04b674811b159569c6aa28b
JulienC-Dev <julien.cormier@yahoo.fr>
2022-01-17 16:53:16 +0100

    modif programme

--- main.py
+++ main.py
@@ -1,40 +1,67 @@
-# action = [
-#     {"action1": (20, 0.05)},
-#     {"action2": (30, 0.10)},
-#     {"action3": (50, 0.15)},
-#     {"action4": (70, 0.20)},
-#     {"action5": (60, 0.17)},
-#     {"action6": (80, 0.25)},
-#     {"action7": (22, 0.07)},
-#     {"action8": (26, 0.11)},
-#     {"action9": (48, 0.13)},
-#     {"action10": (34, 0.27)},
-#     {"action11": (42, 0.17)},
-#     {"action12": (110, 0.09)},
-#     {"action13": (38, 0.23)},
-#     {"action14": (14, 0.01)},
-#     {"action15": (18, 0.03)},
-#     {"action16": (8, 0.08)},
-#     {"action17": (4, 0.12)},
-#     {"action18": (10, 0.14)},
-#     {"action19": (24, 0.21)},
-#     {"action20": (114, 0.18)},
-#     ]
+import csv
+# from itertools import combinations
+import copy
 
-# def calculProfit(action):
-#     n = 500
-#     profit = []
-#     for i in range(len(action)):
-#         for x in action[i].items():
-#             quotient = n//x[1][0]
-#             profit.append(quotient * x[1][1])
-#     return print(profit)
-#
-# calculProfit(action)
+with open("Action.csv", newline="")as csvfile:
+    csvActions = csv.reader(csvfile, delimiter=',', quotechar='|')
+    actions = []
+    next(csvActions)
+    listActions = [[str(i[0]), int(i[1]), float(i[2]), int(i[1]) * float(i[2])] for i in csvActions]
+    Portefeuille = "portefeuille"
+    # p = [[print(portefeuille, (x[0], y[1]))] for x in listActions for y in listActions]
+    nbAction = len(listActions)
+    countPortefeuille = 0
 
+    # def combinations(iterable, r):
+    #         pool = tuple(iterable)
+    #         n = len(pool)
+    #         if r > n:
+    #             return
+    #         indices = list(range(r))
+    #         yield tuple(pool[i] for i in indices)
+    #         while True:
+    #             for i in reversed(range(r)):
+    #                 if indices[i] != i + n - r:
+    #                     break
+    #             else:
+    #                 return
+    #             indices[i] += 1
+    #             for j in range(i + 1, r):
+    #                 indices[j] = indices[j - 1] + 1
+    #
+    #             yield tuple(pool[i] for i in indices)
+    #
+    # listP = list(combinations(listActions, 10))
+    # for i in listP:
+    #     countPortefeuille += 1
+    #     print("portefeuille", countPortefeuille, len(i))
 
+    def combinations(target, listActions):
+        for i in range(len(listActions)):
+            new_target = copy.copy(target)
+            new_data = copy.copy(listActions)
+            new_target.append(listActions[i])
+            new_data = listActions[i + 1:]
+            # print(new_target)
+            combinations(new_target, new_data)
 
-def calculProfit(action):
-    return
+    target = []
+    combinations(target, listActions)
 
+    # for x in listActions:
+    #     for y in listActions:
+    #         for v in listActions:
+    #             if (y[1] == v[1]):
+    #                 break
+    #             for z in listActions:
+    #                 if (v[1] == z[1]):
+    #                     break
+    #                 countPortefeuille += 1
+    #                 print("portefeuille",countPortefeuille,(x[0], y[1], v[1], z[1]))
 
+    # p = [([i for i in (x[0], y[1])]) for x in listActions for y in listActions]
+
+    # def combinations(listActions,20):
+    #     pool = tuple(iterable)
+
+    # product(listActions)
