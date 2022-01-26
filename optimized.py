9777faf3431e907fbedbba36e59d8731dcc8ee27
JulienC-Dev <julien.cormier@yahoo.fr>
2022-01-26 14:42:37 +0100

    optimisation

--- optimized.py
+++ optimized.py
@@ -1,27 +1,42 @@
 import csv
-from itertools import combinations, accumulate
+from itertools import accumulate
 import operator
-from operator import itemgetter
 
 with open("Actions.csv", newline="")as csvfile:
     csvActions = csv.reader(csvfile, delimiter=',', quotechar='|')
     actions = []
     next(csvActions)
-    listActions = [[str(i[0]), int(i[1]), float(i[2]), int(i[1])] for i in csvActions]
+    listActions = [[str(i[0]), int(i[1]), float(i[2]), int(i[1]) * float(i[2])] for i in csvActions]
+    # listportefeuille = []
 
-    maxPortefeuille = 500
-    portefeuille = []
-    i = len(listActions)-1
+    def calcul(listActions):
+        for x in range(len(listActions)-1, -1, -1):
+            listeAc = listActions[:x+1]
+            maxPortefeuille = 500
+            i = len(listeAc)
+            countValeurs = 0
+            portefeuille = []
 
-    while maxPortefeuille > 0 and len(portefeuille) < 20:
-        lAction = listActions[i]
+            while maxPortefeuille > 0 and i > 0:
+                lAction = listeAc[i-1][1]
+                countValeurs += 1
+                if lAction > maxPortefeuille:
+                    i -= 1
+                else:
+                    i -= 1
+                    maxPortefeuille -= lAction
+                    portefeuille.append(listeAc[i])
+                    if maxPortefeuille == 0:
+                        valuePortofolio = list(accumulate([y[1] for y in portefeuille], operator.__add__))
+                        rentabilite = list(accumulate([y[3] for y in portefeuille], operator.__add__))
+                        return print("Meilleur Portefeuille :", countValeurs," - Montant",
+                               valuePortofolio[-1], "euros - rentabilité", rentabilite[-1], "Nom des Actions:",
+                               " ".join([x[0] for x in portefeuille]))
 
-        if lAction[1] > maxPortefeuille:
-            i -= 1
-        else:
-            i -= 1
-            maxPortefeuille -= lAction[1]
-            portefeuille.append(lAction)
-            print(lAction, maxPortefeuille)
 
+            # listportefeuille.append([[x[0] for x in portefeuille], valuePortofolio[-1], rentabilite[-1]])
+
+    calcul(listActions)
+    # for i in listportefeuille:
+    #     print(len(i), i)
 
