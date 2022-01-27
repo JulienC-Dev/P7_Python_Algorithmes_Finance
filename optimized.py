9bf45301ce828b5197a711b7a4355639d22c8fd1
JulienC-Dev <julien.cormier@yahoo.fr>
2022-01-27 11:11:29 +0100

    modification

--- optimized.py
+++ optimized.py
@@ -1,42 +1,43 @@
 import csv
 from itertools import accumulate
 import operator
+from operator import itemgetter
 
-with open("Actions.csv", newline="")as csvfile:
-    csvActions = csv.reader(csvfile, delimiter=',', quotechar='|')
-    actions = []
-    next(csvActions)
-    listActions = [[str(i[0]), int(i[1]), float(i[2]), int(i[1]) * float(i[2])] for i in csvActions]
-    # listportefeuille = []
-
-    def calcul(listActions):
-        for x in range(len(listActions)-1, -1, -1):
-            listeAc = listActions[:x+1]
-            maxPortefeuille = 500
-            i = len(listeAc)
-            countValeurs = 0
-            portefeuille = []
-
-            while maxPortefeuille > 0 and i > 0:
-                lAction = listeAc[i-1][1]
-                countValeurs += 1
-                if lAction > maxPortefeuille:
-                    i -= 1
-                else:
-                    i -= 1
-                    maxPortefeuille -= lAction
-                    portefeuille.append(listeAc[i])
-                    if maxPortefeuille == 0:
-                        valuePortofolio = list(accumulate([y[1] for y in portefeuille], operator.__add__))
-                        rentabilite = list(accumulate([y[3] for y in portefeuille], operator.__add__))
-                        return print("Meilleur Portefeuille :", countValeurs," - Montant",
-                               valuePortofolio[-1], "euros - rentabilité", rentabilite[-1], "Nom des Actions:",
-                               " ".join([x[0] for x in portefeuille]))
+def getData(file):
+    with open(file, newline="")as csvfile:
+        csvActions = csv.reader(csvfile, delimiter=',', quotechar='|')
+        next(csvActions)
+        listActions = [[str(i[0]), int(i[1]), float(i[2]), int(i[1]) * float(i[2])] for i in csvActions]
+        return listActions
 
 
-            # listportefeuille.append([[x[0] for x in portefeuille], valuePortofolio[-1], rentabilite[-1]])
+def calcul(listActions):
+    listeportefeuille = []
+    for x in range(0, len(listActions)):
+        listeAc = listActions[x:]
+        maxPortefeuille = 500
 
-    calcul(listActions)
-    # for i in listportefeuille:
-    #     print(len(i), i)
+        portefeuille = []
+        i = 0
 
+        while maxPortefeuille > 0 and len(listeAc) > i:
+            lAction = listeAc[i][1]
+
+            if lAction > maxPortefeuille:
+                i += 1
+            else:
+                maxPortefeuille -= lAction
+                portefeuille.append(listeAc[i])
+                i += 1
+
+        valuePortofolio = list(accumulate([y[1] for y in portefeuille], operator.__add__))
+        rentabilite = list(accumulate([y[3] for y in portefeuille], operator.__add__))
+        listeportefeuille.append([[x[0] for x in portefeuille], valuePortofolio[-1], rentabilite[-1]])
+    MeilleurPortefeuille = sorted(listeportefeuille, key=lambda montant: [montant[1] for x in listeportefeuille],
+                                  reverse=True)
+    print(MeilleurPortefeuille[0])
+
+
+if __name__ == "__main__":
+    FileActions = "Actions.csv"
+    calcul(getData(FileActions))
